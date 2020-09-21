7626786b005c08b7d59f5c81498a412b9e1d9abb
yixueping <664665458@qq.com>
2020-09-21 10:27:07 +0800

    Merge branch 'lisi' into master

--- src/Header.js
+++ src/Header.js
@@ -4,7 +4,11 @@ export default class Header extends Component {
     render() {
         return (
             <div>
+<<<<<<< HEAD
+                我是deve
+=======
                 我是李四
+>>>>>>> lisi
             </div>
         )
     }
