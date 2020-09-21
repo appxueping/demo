c0b4ed0bf89d62b14db3f8bee795fcf7878ed924
yixueping <664665458@qq.com>
2020-09-21 09:55:13 +0800

    aaa

---
+++ src/aaaa.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class aaaa extends Component {
+    render() {
+        return (
+            <div>
+                
+            </div>
+        )
+    }
+}
